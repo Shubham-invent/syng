7b0f9edbae4ac076f2ed9522f339204e642fd07e
spandey-databridge <spandey@databridgeconsultants.com>
2023-04-24 01:42:59 +0530

    updated

--- bin/server.js
+++ bin/server.js
@@ -61,5 +61,5 @@ function onError(error) {
 function onListening() {
   var addr = server.address();
   var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-  debug("Listening on addr" + bind);
+  debug("Listening on" + bind);
 }
